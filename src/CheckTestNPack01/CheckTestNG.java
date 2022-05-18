c9b49dbff3fde400946eb50465037ac22e43b575
rajeshDnare <rajeshnare@gmail.com>
2022-05-18 11:32:30 +0530

    commit

---
+++ src/CheckTestNPack01/CheckTestNG.java
@@ -0,0 +1,29 @@
+package CheckTestNPack01;
+
+import org.testng.annotations.Test;
+
+public class CheckTestNG {
+	@Test
+	public void pro01() {
+		System.out.println("pro01");
+
+	}
+
+	@Test
+	public void pro02() {
+		System.out.println("pro02");
+
+	}
+
+	@Test
+	public void pro03() {
+		System.out.println("pro03");
+
+	}
+	@Test
+	public void met04() {
+		System.out.println("met04");
+
+	}
+
+}
